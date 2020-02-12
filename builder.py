a5744614d4e619d3d1268e231e81248c36ce02ca
David Schneider <schdav@users.noreply.github.com>
2020-02-12 18:33:31 +0100

    Add article object

--- builder.py
+++ builder.py
@@ -1,29 +1,15 @@
-"""Build module for Blogy."""
-
 import fileinput
-import math
 import os
 from datetime import date as d
 from shutil import copyfile
 
 import sass
-from markdown import markdown
 
 import helpers
-
-
-def _calculate_time_to_read(text):
-    """Calculate time to read of given text."""
-    word_count = len(text.split())
-    minutes = math.ceil(word_count / 150)
-    str_minutes = 'minutes' if minutes > 1 else 'minute'
-
-    return '{} {}'.format(minutes, str_minutes)
+from article import Article
 
 
 class Builder:
-    """Builder to build blog."""
-
     def __init__(self, **kwargs):
         self.selected_theme = kwargs.get('theme')
         self.blog_name = kwargs.get('name')
@@ -34,80 +20,65 @@ class Builder:
         theme_file = 'themes/{}.scss'.format(self.selected_theme)
         helpers.check_file(theme_file)
 
+        self._compile_theme(theme_file)
+
+    def _compile_theme(self, theme_file):
         scss = open(theme_file, 'r')
         css = open('build/{}.css'.format(str.lower(self.selected_theme)), 'w')
         css.write(sass.compile(string=scss.read()))
         scss.close()
         css.close()
 
-    def build_article(self, article):
-        """Build given article."""
-        data = helpers.load_yaml(article)
+    def build_article(self, article_file):
+        article = Article(article_file)
 
-        title = helpers.read_key(data[0], 'title')
-        date = helpers.read_key(data[0], 'date')
-        is_publish = helpers.read_key(data[0], 'publish')
+        if not article.is_publish:
+            return
 
-        subfolder = str(date).replace('-', '/')
-        name = str.lower(os.path.splitext(article)[0])
+        subfolder = '../build/{}'.format(article.get_subfolder())
+        os.makedirs(subfolder, exist_ok=True)
 
-        formatted_date = date.strftime('%m/%d/%y')
-
-        if is_publish:
-            folder = '../build/{}'.format(subfolder)
-            os.makedirs(folder, exist_ok=True)
-            self._create_html(folder, name, data)
-
-            self.blog_entries.append(
-                '<li><a href="{}/{}.html"> \
-                    <span class="date">{}</span>{} \
-                        </a></li>\n'.format(
-                            subfolder, name, formatted_date, title))
-
-    def _create_html(self, folder, name, data):
-        """Create html of given article."""
-        file = '{}/{}.html'.format(folder, name)
+        file = '{}/{}.html'.format(subfolder, article.name)
         template = '../templates/article.html'
 
         helpers.check_file(template)
         copyfile(template, file)
 
-        article_markdown = helpers.read_key(data[1], 'markdown')
-        text = markdown(article_markdown, output_format='html5',
-                        extensions=['markdown.extensions.sane_lists'])
-
-        time_to_read = _calculate_time_to_read(text)
-
-        title = helpers.read_key(data[0], 'title')
-        date = helpers.read_key(data[0], 'date')
-
-        formatted_date = date.strftime('%m/%d/%y')
-
         with fileinput.FileInput(file, inplace=1) as file_input:
             for line in file_input:
                 print(line.replace('{{ theme }}', '../../../{}.css'.format(
                     str.lower(self.selected_theme))).
-                      replace('{{ title }}', title).
-                      replace('{{ date }}', str(formatted_date)).
-                      replace('{{ text }}', text).
-                      replace('{{ time_to_read }}', time_to_read).
+                      replace('{{ title }}', article.title).
+                      replace('{{ date }}', article.get_formatted_date()).
+                      replace('{{ text }}', article.text).
+                      replace('{{ time_to_read }}',
+                              article.calculate_time_to_read()).
                       replace('{{ blog_name }}', self.blog_name).
                       replace('{{ year }}', str(d.today().year)).
                       replace('{{ language }}', str.lower(self.language)),
                       end='')
 
         helpers.minify_html(file)
+        self.blog_entries.append(article)
 
     def build_overview(self):
-        """Build overview."""
         file = '../build/index.html'
         template = '../templates/overview.html'
 
         helpers.check_file(template)
         copyfile(template, file)
 
-        self.blog_entries.sort(reverse=True)
-        entries_html = ''.join(self.blog_entries)
+        entries_html = ''
+        self.blog_entries.sort(key=lambda x: x.date, reverse=True)
+
+        for blog_entry in self.blog_entries:
+            entries_html += '<li><a href="{}/{}.html"> \
+                    <span class="date">{}</span>{} \
+                        </a></li>\n'.format(
+                            blog_entry.get_subfolder(),
+                            blog_entry.name,
+                            blog_entry.get_formatted_date(),
+                            blog_entry.title)
 
         with fileinput.FileInput(file, inplace=1) as file_input:
             for line in file_input:
